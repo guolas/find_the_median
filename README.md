bbf4d91518565387d85a999482fc3e27d7c1169a
guolas <guolas@gmail.com>
2016-02-20 12:48:40 +0100

    First commit, just reading the input

---
+++ README.md
@@ -0,0 +1,1 @@
+[Find the Median](https://www.hackerrank.com/challenges/find-median) problem in HackerRank.
---
+++ median.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+"""
+Get the size of the array
+"""
+N = int(input().strip())
+
+"""
+Get the array
+"""
+A = [int(x) for x in input().strip().split(" ")]
+
+print(A)
